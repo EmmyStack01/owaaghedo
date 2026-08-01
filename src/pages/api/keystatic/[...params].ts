c5bee5df42394da054391f77e7b5d8155dcb52b1
Emmy STACK01 <wezuduemoih@gmail.com>
2026-07-31 18:07:54 -0700

    Update [...params].ts

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,4 +1,6 @@
 import { makeHandler } from '@keystatic/astro/api';
-import keystaticConfig from '~/keystatic.config';
+import keystaticConfig from '../../../../keystatic.config';
+
+export const prerender = false;
 
 export const ALL = makeHandler({ config: keystaticConfig });
