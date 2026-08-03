813d0a5cfe1687f0121e275ccd242ffd1e1d9896
Emmy STACK01 <wezuduemoih@gmail.com>
2026-08-03 06:41:20 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://owaaghedo.pages.dev',
+  trailingSlash: 'ignore',
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough',
