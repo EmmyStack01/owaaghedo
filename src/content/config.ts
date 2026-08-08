07c272b7d6c5e121f9765d5f548c4de9b996cf01
Emmy STACK01 <wezuduemoih@gmail.com>
2026-08-08 02:24:21 +0100

    Update file loader pattern to include .mdx files

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,8 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const articles = defineCollection({
-  // Updated pattern to catch both .mdoc and .json files
-  loader: glob({ pattern: '**/*.{json,mdoc}', base: './src/content/articles' }),
+  loader: glob({ pattern: '**/*.{mdx,mdoc,json}', base: './src/content/articles' }),
   schema: z.object({
     title: z.string(),
     authorName: z.string().default('Owa Aghedo'),
